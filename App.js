c2e8df81ac270a850edce0714f5502dd4e0d8937
deve.hoge@gmail.com <61268214+dev-mori@users.noreply.github.com>
2020-12-05 18:28:44 +0900

    initialize

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, paddingVertical: 80, alignItems: "center" }}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello</Text>
+      <Button
+        title="Click Me"
+        onPress={() => {
+          alert("アラート表示");
+          console.log("コンソールログ");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
